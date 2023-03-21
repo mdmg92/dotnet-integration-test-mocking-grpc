c40b86a8a57fbb0d0e54c86c90169a6634ba16fb
Mathias Ariel de Mestral Granada <mathias.demestral@bancontinental.com.py>
2023-03-21 10:38:48 -0300

    Consume grpc service

---
+++ src/WebApi/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using GrpcService;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace WebApi;
+
+[ApiController]
+[Route("/")]
+public class HomeController : ControllerBase
+{
+    private readonly Greeter.GreeterClient _greeterClient;
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(Greeter.GreeterClient greeterClient, ILogger<HomeController> logger)
+    {
+        _greeterClient = greeterClient;
+        _logger        = logger;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Get([FromQuery] string name)
+    {
+        _logger.LogInformation("-- init request");
+
+        return Ok(await _greeterClient.SayHelloAsync(new HelloRequest { Name = name }));
+    }
+}
--- src/WebApi/Program.cs
+++ src/WebApi/Program.cs
@@ -1,6 +1,16 @@
+using GrpcService;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+builder.Services.AddGrpcClient<Greeter.GreeterClient>(options =>
+{
+    options.Address = new Uri(builder.Configuration.GetConnectionString("GrpcService"));
+});
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapControllers();
 
 app.Run();
